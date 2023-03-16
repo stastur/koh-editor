752030a196e310d23625cb1a4be5429aaad963ef
Stanislau Tur <stanislau.tur@gmail.com>
2023-03-16 15:04:53 +0100

    add geometry utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,50 @@
 import type { Point } from "./store";
 
+type Line = [Point, Point];
+
 export function clamp(value: number, min: number, max: number) {
   return Math.min(Math.max(min, value), max);
 }
 
 export function distance(from: Point, to: Point): number {
   return Math.hypot(to.x - from.x, to.y - from.y);
+}
+
+export function angle(from: Point, to: Point): number {
+  return Math.atan2(to.y - from.y, to.x - from.x);
+}
+
+export function angleBetweenLines(l1: Line, l2: Line) {
+  return angle(...l1) - angle(...l2);
+}
+
+// y = ax + b
+export function closestLinePoint(p: Point, l: Line) {
+  const [start, end] = l;
+  const al = (end.y - start.y) / (end.x - start.x);
+  const bl = start.y - al * start.x;
+
+  const a = -1 / al;
+  const b = p.y - a * p.x;
+
+  const x = (b - bl) / (al - a);
+  const y = a * p.x + b;
+
+  if (
+    x >= Math.min(start.x, end.x) &&
+    x <= Math.max(start.x, end.x) &&
+    y >= Math.min(start.y, end.y) &&
+    y <= Math.max(start.y, end.y)
+  ) {
+    return { x, y };
+  } else {
+    const d1 = distance(p, start);
+    const d2 = distance(p, end);
+
+    return d1 < d2 ? start : end;
+  }
+}
+
+export function distanceToLine(p: Point, line: Line) {
+  return distance(p, closestLinePoint(p, line));
 }
