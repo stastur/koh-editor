ad1e496a3cf8af766d77437b823002b695ce5f08
Stanislau Tur <stanislau.tur@gmail.com>
2023-04-09 22:36:34 +0200

    Define tool strategy

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -4,57 +4,35 @@
   import type { Options } from "roughjs/bin/core";
   import { P, match } from "ts-pattern";
   import { onMount } from "svelte";
-  import { deleteObject, newArc, newArcPoint, newPosition } from "./actions";
-  import { CursorType, MouseButtons } from "./constants";
-  import {
-    activeTool,
-    editingElement,
-    hoveredElement,
-    selection,
-    state,
-    type Point,
-    type Tool,
-  } from "./store";
-  import {
-    distance,
-    isCloseToPolyline,
-    roundPoint,
-    subtract,
-    toScene,
-    toTuple,
-    add,
-    clamp,
-    multiply,
-    divide,
-  } from "./utils";
+  import { deleteObject } from "./actions";
+  import { activeTool, editingElement, selection, state } from "./store";
+  import { toTuple, add, multiply } from "./utils";
   import { derived } from "svelte/store";
+  import {
+    context,
+    cursor,
+    Editor,
+    hovered,
+    selected,
+    strategies,
+    viewport,
+    zoom,
+  } from "./editor";
 
   let canvas: HTMLCanvasElement;
   let ctx: CanvasRenderingContext2D;
   let height = 1024;
   let width = 1024;
+  let editor: Editor;
 
   let rc: RoughCanvas;
-
-  let viewport = { x: 0, y: 0, w: width, h: height };
-  let cursorPosition = { x: 0, y: 0 };
-  let isDragging = false;
-  let isPanning = false;
-  let zoom = 1;
 
   $: canUndo = derived(state.redoState, (s) => s.canUndo);
   $: canRedo = derived(state.redoState, (s) => s.canRedo);
 
-  const cursorsForTool: Record<Tool, string> = {
-    arc: CursorType.crosshair,
-    hand: CursorType.grab,
-    select: CursorType.auto,
-    position: CursorType.crosshair,
-  };
-
-  $: cursor = cursorsForTool[$activeTool];
+  $: editor && (editor.strategy = strategies[$activeTool]);
   $: viewportPoints = $state.points.map((p) =>
-    add(multiply(p, zoom), viewport)
+    add(multiply(p, $zoom), $viewport)
   );
 
   onMount(() => {
@@ -63,142 +41,20 @@
     ctx = canvas.getContext("2d")!;
     ctx.scale(devicePixelRatio, devicePixelRatio);
 
+    editor = new Editor(canvas, context);
+
     rc = r.canvas(canvas, { options: { seed: 420, roughness: 0 } });
   });
-
-  const elementToCanvasCoords = (p: Point) =>
-    toScene(p, canvas, canvas.getBoundingClientRect());
-
-  const canvasToElementCoords = (p: Point) =>
-    toScene(p, canvas.getBoundingClientRect(), canvas);
-
-  function handleMove(e: MouseEvent) {
-    cursorPosition = roundPoint(
-      divide(
-        subtract(
-          elementToCanvasCoords({ x: e.offsetX, y: e.offsetY }),
-          viewport
-        ),
-        zoom
-      )
-    );
-
-    if ($activeTool === "select") {
-      if (isDragging) {
-        const selectedObj = $state.objects[$selection];
-
-        const draggingIndex = selectedObj.points.find(
-          (i) => distance($state.points[i], cursorPosition) < 10
-        );
-
-        if (draggingIndex !== undefined) {
-          state.update(
-            ({ points }) => (points[draggingIndex] = cursorPosition)
-          );
-        }
-      } else {
-        const elementUnder = $state.objects.findIndex((o) => {
-          const coords = o.points.map((i) => $state.points[i]);
-
-          if (o.type === "position") {
-            return distance(cursorPosition, coords[0]) < 5;
-          }
-
-          if (o.type === "arc") {
-            return isCloseToPolyline(cursorPosition, coords);
-          }
-        });
-
-        hoveredElement.set(elementUnder);
-      }
-    }
-
-    if ($activeTool === "hand" && isPanning) {
-      const delta = elementToCanvasCoords({ x: e.movementX, y: e.movementY });
-
-      viewport = { ...viewport, ...add(viewport, delta) };
-      viewport.x = clamp(viewport.x, width - viewport.w * zoom, 0);
-      viewport.y = clamp(viewport.y, height - viewport.h * zoom, 0);
-    }
-
-    if ($activeTool === "arc") {
-      const closePoint = $state.points.find(
-        (p) => distance(p, cursorPosition) < 10
-      );
-      if (closePoint) {
-        cursorPosition = closePoint;
-      }
-    }
-  }
-
-  function handleMouseDown(e: MouseEvent) {
-    if (e.button !== MouseButtons.main) {
-      return;
-    }
-
-    if ($activeTool === "hand") {
-      isPanning = true;
-      cursor = CursorType.grabbing;
-    }
-
-    if ($activeTool === "select") {
-      if ($selection !== -1) {
-        isDragging = true;
-      }
-    }
-  }
-
-  function handleMouseUp(e: MouseEvent) {
-    if (e.button !== MouseButtons.main) {
-      return;
-    }
-
-    if ($activeTool === "hand") {
-      cursor = CursorType.grab;
-    }
-
-    if ($activeTool === "position") {
-      newPosition(cursorPosition);
-      state.commit();
-    }
-
-    if ($activeTool === "arc") {
-      if ($editingElement === -1) {
-        $editingElement = $state.objects.length;
-        newArc();
-      }
-
-      newArcPoint($editingElement, cursorPosition);
-
-      const { points } = $state.objects[$editingElement];
-      if (points.length >= 2 && points.at(0) === points.at(-1)) {
-        $editingElement = -1;
-      }
-
-      state.commit();
-    }
-
-    if ($activeTool === "select") {
-      if (isDragging) {
-        state.commit();
-      } else {
-        $selection = $hoveredElement;
-      }
-    }
-
-    isPanning = false;
-    isDragging = false;
-  }
 
   $: if (ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     $state.objects.forEach((o, i) => {
       const options: Options = {};
-      const selected = $selection === i;
-      const hovered = $hoveredElement === i;
+      const isSelected = $selected === i;
+      const isHovered = $hovered === i;
 
-      if (selected || hovered) {
+      if (isSelected || isHovered) {
         options.stroke = "blue";
       }
 
@@ -211,7 +67,7 @@
         const coords = o.points.map((i) => viewportPoints[i]);
 
         if ($editingElement === i) {
-          coords.push(add(multiply(cursorPosition, zoom), viewport));
+          coords.push(add(multiply($cursor.position, $zoom), $viewport));
         }
 
         rc.linearPath(
@@ -219,7 +75,7 @@
           options
         );
 
-        if (selected) {
+        if (isSelected) {
           coords.forEach((p) =>
             rc.circle(...toTuple(p), 8, {
               fill: "white",
@@ -251,24 +107,12 @@
       .with({ key: P.union("Delete", "Backspace") }, () => {
         if ($selection !== -1) {
           deleteObject($selection);
-          $selection = -1;
-          $editingElement = -1;
           state.commit();
         }
       })
       .with({ metaKey: true, shiftKey: true, key: "z" }, () => state.redo())
       .with({ metaKey: true, key: "z" }, () => state.undo())
       .otherwise(() => {});
-  }
-
-  function handleMouseWheel(e: WheelEvent) {
-    const newZoom = Number((zoom + Math.sign(e.deltaY) * 1).toPrecision(2));
-    zoom = clamp(newZoom, 1, 8);
-
-    const offset = subtract(multiply(cursorPosition, zoom), cursorPosition);
-
-    viewport.x = -offset.x;
-    viewport.y = -offset.y;
   }
 </script>
 
@@ -278,15 +122,15 @@
   <canvas
     bind:this={canvas}
     class="h-full aspect-square"
-    style:cursor
+    style:cursor={$cursor.type}
     on:contextmenu|preventDefault={clearFocus}
-    on:mouseup={handleMouseUp}
-    on:mousedown={handleMouseDown}
-    on:mousemove={handleMove}
-    on:wheel={handleMouseWheel}
+    on:mousemove={editor.onMouseMove}
+    on:mousedown={editor.onMouseDown}
+    on:mouseup={editor.onMouseUp}
+    on:wheel={editor.onWheel}
   />
   <div class="absolute m-4 p-2 left-0 bottom-0">
-    <span>{zoom * 100}%</span>
+    <span>{$zoom * 100}%</span>
     <button class="btn" disabled={!$canUndo} on:click={state.undo}>undo</button>
     |
     <button class="btn" disabled={!$canRedo} on:click={state.redo}>redo</button>
---
+++ src/lib/editor.ts
@@ -0,0 +1,248 @@
+import { get, writable } from "svelte/store";
+import { state, type Point, type Tool, activeTool } from "./store";
+import { CursorType } from "./constants";
+import {
+  clamp,
+  distance,
+  divide,
+  isCloseToPolyline,
+  multiply,
+  roundPoint,
+  subtract,
+  toScene,
+} from "./utils";
+import { newArcPoint, newPosition } from "./actions";
+
+export const zoom = writable(1);
+export const selected = writable(-1);
+export const hovered = writable(-1);
+export const viewport = writable({ x: 0, y: 0, width: 0, height: 0 });
+
+export const cursor = writable({
+  type: CursorType.auto,
+  position: { x: 0, y: 0 },
+});
+
+const cursorsForTool: Record<Tool, string> = {
+  arc: CursorType.crosshair,
+  hand: CursorType.grab,
+  select: CursorType.auto,
+  position: CursorType.crosshair,
+};
+
+activeTool.subscribe((tool) =>
+  cursor.update((cursor) => {
+    cursor.type = cursorsForTool[tool];
+    return cursor;
+  })
+);
+
+export const context = {
+  zoom,
+  cursor,
+  viewport,
+  selected,
+  hovered,
+};
+
+interface ToolStrategy {
+  mouseUp?(cursor: Point, _event: MouseEvent): void;
+  mouseDown?(cursor: Point, _event: MouseEvent): void;
+  mouseMove?(cursor: Point, _event: MouseEvent): void;
+  keyDown?(_event: KeyboardEvent): void;
+}
+
+class HandStrategy implements ToolStrategy {
+  previousCursorPosition = { x: 0, y: 0 };
+
+  constructor(private ctx: typeof context) {}
+
+  mouseMove(cursor: Point, event: MouseEvent) {
+    if (get(this.ctx.cursor).type === CursorType.grab) {
+      this.previousCursorPosition = cursor;
+      return;
+    }
+
+    const delta = { x: event.movementX, y: event.movementY };
+
+    this.ctx.viewport.update((viewport) => {
+      viewport.x += delta.x;
+      viewport.y += delta.y;
+      return viewport;
+    });
+
+    this.previousCursorPosition = cursor;
+  }
+
+  mouseDown() {
+    this.ctx.cursor.update((cursor) => {
+      cursor.type = CursorType.grabbing;
+      return cursor;
+    });
+  }
+
+  mouseUp() {
+    this.ctx.cursor.update((cursor) => {
+      cursor.type = CursorType.grab;
+      return cursor;
+    });
+  }
+}
+
+class ArcStrategy implements ToolStrategy {
+  objIdx = -1;
+
+  constructor(private ctx: typeof context) {}
+
+  mouseUp(cursor: Point): void {
+    if (this.objIdx === -1) {
+      state.update(({ objects }) => {
+        this.objIdx = objects.length;
+        objects.push({ type: "arc", points: [], properties: {} });
+      });
+    }
+
+    newArcPoint(this.objIdx, cursor);
+
+    state.update(({ objects }) => {
+      const arc = objects[this.objIdx];
+      const first = arc.points.at(0);
+      const last = arc.points.at(-1);
+
+      if (arc.points.length > 2 && first === last) {
+        this.objIdx = -1;
+      }
+    });
+
+    state.commit();
+  }
+}
+
+class PositionStrategy implements ToolStrategy {
+  constructor(private ctx: typeof context) {}
+
+  mouseUp(cursor: Point): void {
+    newPosition(cursor);
+    state.commit();
+  }
+}
+
+class SelectStrategy implements ToolStrategy {
+  dragging = -1;
+
+  constructor(private ctx: typeof context) {}
+
+  mouseMove(cursor: Point): void {
+    const { objects, points } = get(state);
+
+    const elementUnder = objects.findIndex((o) => {
+      const coords = o.points.map((i) => points[i]);
+
+      if (o.type === "position") {
+        return distance(cursor, coords[0]) < 5;
+      }
+
+      if (o.type === "arc") {
+        return isCloseToPolyline(cursor, coords);
+      }
+    });
+
+    this.ctx.hovered.set(elementUnder);
+
+    this.ctx.cursor.update((cursor) => {
+      cursor.type =
+        get(this.ctx.hovered) === -1 ? CursorType.auto : CursorType.pointer;
+      return cursor;
+    });
+
+    if (this.dragging !== -1) {
+      state.update(({ points }) => (points[this.dragging] = cursor));
+    }
+  }
+
+  mouseUp(): void {
+    this.ctx.selected.set(get(this.ctx.hovered));
+
+    if (this.dragging !== -1) {
+      this.dragging = -1;
+      state.commit();
+    }
+  }
+
+  mouseDown(cursor: Point): void {
+    const { objects, points } = get(state);
+    const selected = get(this.ctx.selected);
+
+    if (selected !== -1 && this.dragging === -1) {
+      const selectedObj = objects[selected];
+      const draggingIndex = selectedObj.points.find(
+        (i) => distance(points[i], cursor) < 10
+      );
+
+      this.dragging = draggingIndex ?? -1;
+    }
+  }
+}
+
+export class Editor {
+  strategy!: ToolStrategy;
+
+  constructor(private canvas: HTMLCanvasElement, private ctx: typeof context) {}
+
+  elementToCanvasCoords = (p: Point) =>
+    toScene(p, this.canvas, this.canvas.getBoundingClientRect());
+
+  onWheel = ({ deltaY }: WheelEvent) => {
+    this.ctx.zoom.update((zoom) => {
+      const newZoom = Number((zoom + Math.sign(deltaY) * 1).toPrecision(2));
+      return clamp(newZoom, 1, 8);
+    });
+
+    this.ctx.viewport.update((viewport) => {
+      const cursor = get(this.ctx.cursor).position;
+      const offset = subtract(multiply(cursor, get(zoom)), cursor);
+
+      viewport.x = -offset.x;
+      viewport.y = -offset.y;
+
+      return viewport;
+    });
+  };
+
+  onMouseDown = (event: MouseEvent) => {
+    this.strategy.mouseDown?.(get(this.ctx.cursor).position, event);
+  };
+
+  onMouseUp = (event: MouseEvent) => {
+    this.strategy.mouseUp?.(get(this.ctx.cursor).position, event);
+  };
+
+  onMouseMove = (event: MouseEvent) => {
+    this.ctx.cursor.update((cursor) => {
+      cursor.position = roundPoint(
+        divide(
+          subtract(
+            this.elementToCanvasCoords({ x: event.offsetX, y: event.offsetY }),
+            get(viewport)
+          ),
+          get(zoom)
+        )
+      );
+
+      return cursor;
+    });
+
+    this.strategy.mouseMove?.(get(this.ctx.cursor).position, event);
+  };
+
+  onKeyDown = (event: KeyboardEvent) => {
+    this.strategy.keyDown?.(event);
+  };
+}
+
+export const strategies: Record<Tool, ToolStrategy> = {
+  arc: new ArcStrategy(context),
+  position: new PositionStrategy(context),
+  select: new SelectStrategy(context),
+  hand: new HandStrategy(context),
+};
